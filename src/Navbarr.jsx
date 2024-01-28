bc468d9114f79c3466ab4b6025f9ae618ad6eb32
pankaj <yadav@gmaile.com>
2024-01-28 13:07:41 +0530

    first commit

---
+++ src/Navbarr.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { Outlet, Link } from "react-router-dom";
+export default function Navbarr(){
+ return(
+    <>
+        <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+
+ )
+}
